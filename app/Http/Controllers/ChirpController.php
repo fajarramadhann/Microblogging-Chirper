9dd7f6eb2afb8d34744bd5c1663977a1eebe69e2
fajarramadhann <ramadhan28.fajar@gmail.com>
2024-05-16 19:17:30 +0700

    First commit

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\View\View;
+
+class ChirpController extends Controller
+{
+    public function index(): View
+    {
+        return view ('chirps', [
+                
+        ]);
+    }
+}
