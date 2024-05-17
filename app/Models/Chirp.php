91860f7e7e5a8e37cf3e5bfdb25b5d6fad02239c
fajarramadhann <ramadhan28.fajar@gmail.com>
2024-05-17 08:19:25 +0700

    feat: Add ChirpCreated event to Chirp model

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -2,6 +2,7 @@
 
 namespace App\Models; // Nentuin file class Chirp apa dimana
 
+use App\Events\ChirpCreated; // Menggunakan class ChirpCreated
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -12,6 +13,10 @@ class Chirp extends Model // Class Chirp extend ke class Model buat dapetin func
 
     protected $fillable = [ // memproteksi dan mengizinkan/mendaftarkan kolom 'message' untuk diisi dan dimasukkan datanya ke database
         'message',
+    ];
+
+    protected $dispatchEvents = [ // membuat event ChirpCreated dan event ChirpCreated akan dipanggil ketika ada data baru di database
+        'created' => ChirpCreated::class,
     ];
 
         public function user(): BelongsTo // membuat function/method user() agar bisa digunakan secara public atau diluar class Chirp
