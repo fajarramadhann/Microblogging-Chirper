9a436844b5bf0ef8314a4b257281b24fd7180948
fajarramadhann <ramadhan28.fajar@gmail.com>
2024-05-17 07:19:39 +0700

    add delete Chirp feature

--- resources/views/livewire/chirps/list.blade.php
+++ resources/views/livewire/chirps/list.blade.php
@@ -25,6 +25,14 @@ $edit = function (Chirp $chirp) {
     $this->getChirps();
 };
 
+$delete = function (Chirp $chirp) {
+    $this->authorize('delete', $chirp);
+
+    $chirp->delete();
+
+    $this->getChirps();
+};
+
 ?>
 
 <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
@@ -55,6 +63,9 @@ $edit = function (Chirp $chirp) {
                                 <x-dropdown-link wire:click="edit({{ $chirp->id }})">
                                     {{ __('Edit') }}
                                 </x-dropdown-link>
+                                <x-dropdown-link wire:click="delete({{ $chirp->id }})" wire:confirm="Yakin mau hapus chirp ini?"> 
+                                    {{ __('Delete') }}
+                                </x-dropdown-link> 
                             </x-slot>
                         </x-dropdown>
                 @endif
